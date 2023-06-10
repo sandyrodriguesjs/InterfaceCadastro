6af518f20275c57e392e0b681ed55bf9b8b43ec0
sandyrodriguesjs <122131843+sandyrodriguesjs@users.noreply.github.com>
2023-06-10 15:05:49 -0300

    dados do aluno

---
+++ tela/Aluno.java
@@ -0,0 +1,51 @@
+package tela;
+
+public class Aluno {
+
+	private String nome;
+	private String cpf;
+	private String matricula;
+	private String vertente;
+
+	public Aluno(String nome, String cpf, String matricula, String vertente) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.matricula = matricula;
+		this.vertente = vertente;
+	}
+
+	// Getters e Setters
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getVertente() {
+		return vertente;
+	}
+
+	public void setVertente(String vertente) {
+		this.vertente = vertente;
+	}
+
+}
